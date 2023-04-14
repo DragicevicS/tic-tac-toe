1a9087107df0940f7538f8138dda164d5730844c
DragicevicS <steffan95@live.com>
2023-04-14 21:43:29 +0200

    Change the game from console version to browser version, need to add a few features for better UX

--- game.js
+++ game.js
@@ -1,16 +1,6 @@
-const gameBoard = (() => { // setting up the board and it's functions
-  const board = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-  ];
-  const printBoard = () => console.table(board);
-  const getBoard = () => board;
-  return { getBoard, printBoard };
-})();
+const game = (() => {
 
-const players = (() => { // defining players and their functions
-  const player = [
+  const player = [ // list  of players
     {
       name: 'p1',
       playerSymbol: 'X'
@@ -24,50 +14,51 @@ const players = (() => { // defining players and their functions
       playerSymbol: null
     }
   ];
-  let activePlayer = player[0];
-  const switchPlayer = () => {
-    activePlayer = activePlayer === player[0] ? player[1] : player[0];
-  };
+
+  let activePlayer = player[0]; 
+  const switchPlayer = () => activePlayer = activePlayer === player[0] ? player[1] : player[0];
   const getActivePlayer = () => activePlayer;
-  return { switchPlayer, getActivePlayer }
+
+  const playerTurn = document.querySelector('.player-turn');
+  const boardContainer = document.querySelector('.board-container');
+  playerTurn.textContent = `${getActivePlayer().name}'s turn`;
+  
+  for (let i=0; i < 9; i++) { // setting 9 buttons as the board
+    const cellButton = document.createElement('button');
+    cellButton.setAttribute('id', i);
+    boardContainer.appendChild(cellButton);
+  };
+
+  function checkResult() { // function with game logic and checking if there is a winner or a draw
+    let bs = boardContainer.children;
+    let cps = getActivePlayer().playerSymbol;
+    if (bs[0].textContent == cps && bs[4].textContent == cps && bs[8].textContent == cps) {switchPlayer(); setTimeout(() => {alert(`${getActivePlayer().name} won!`); location.reload();}, 50);};
+    if (bs[2].textContent == cps && bs[4].textContent == cps && bs[6].textContent == cps) {switchPlayer(); setTimeout(() => {alert(`${getActivePlayer().name} won!`); location.reload();}, 50);};
+    if (bs[0].textContent == cps && bs[1].textContent == cps && bs[2].textContent == cps) {switchPlayer(); setTimeout(() => {alert(`${getActivePlayer().name} won!`); location.reload();}, 50);};
+    if (bs[3].textContent == cps && bs[4].textContent == cps && bs[5].textContent == cps) {switchPlayer(); setTimeout(() => {alert(`${getActivePlayer().name} won!`); location.reload();}, 50);};
+    if (bs[6].textContent == cps && bs[7].textContent == cps && bs[8].textContent == cps) {switchPlayer(); setTimeout(() => {alert(`${getActivePlayer().name} won!`); location.reload();}, 50);};
+    if (bs[0].textContent == cps && bs[3].textContent == cps && bs[6].textContent == cps) {switchPlayer(); setTimeout(() => {alert(`${getActivePlayer().name} won!`); location.reload();}, 50);};
+    if (bs[1].textContent == cps && bs[4].textContent == cps && bs[7].textContent == cps) {switchPlayer(); setTimeout(() => {alert(`${getActivePlayer().name} won!`); location.reload();}, 50);};
+    if (bs[2].textContent == cps && bs[5].textContent == cps && bs[8].textContent == cps) {switchPlayer(); setTimeout(() => {alert(`${getActivePlayer().name} won!`); location.reload();}, 50);};
+    if (bs[0].textContent != '' && bs[1].textContent != '' && bs[2].textContent != '' && bs[3].textContent != '' && bs[4].textContent != '' && bs[5].textContent != '' && bs[6].textContent != '' && bs[7].textContent != '' && bs[8].textContent != '') return setTimeout(() => {alert(`It' a draw!`); location.reload();}, 50);
+  };
+  const makeMove = (a) => { // core function for every move, with game logic
+    if (boardContainer.children[a].textContent == 'X' || boardContainer.children[a].textContent == 'O') { // checks if the cell is empty
+      playerTurn.textContent = 'Invalid move';
+      setTimeout(() => {playerTurn.textContent = `${getActivePlayer().name}'s turn`}, 1300);
+    } else {   
+      boardContainer.children[a].textContent =  getActivePlayer().playerSymbol; // updates cell's value if it's empty
+      checkResult(); // checks if the game is finished
+      switchPlayer(); // switches players if the game is not finished
+      playerTurn.textContent = `${getActivePlayer().name}'s turn`; // updates the player's name
+    };
+  };
+
+  function clickHandlerBoard(e) { // function for returning if of selected cell
+    const selectedCell = e.target.id;
+    if (!selectedCell) return;
+    makeMove(selectedCell);
+  };
+  boardContainer.addEventListener('click', clickHandlerBoard);
+  
 })();
-
-const gameFlow = () => { // game factory function
-  const board = gameBoard.getBoard();
-
-  gameBoard.printBoard();
-  console.log(`${players.getActivePlayer().name}'s turn`);
-
-  function checkResult(boardState, currentPlayer){
-    let bs = boardState;
-    let cps = currentPlayer.playerSymbol;
-    if (bs[0][0] == cps && bs[1][1] == cps && bs[2][2] == cps) return true;
-    if (bs[0][2] == cps && bs[1][1] == cps && bs[2][0] == cps) return true;
-    if (bs[0][0] == cps && bs[1][0] == cps && bs[2][0] == cps) return true;
-    if (bs[0][1] == cps && bs[1][1] == cps && bs[0][2] == cps) return true;
-    if (bs[0][2] == cps && bs[1][2] == cps && bs[2][2] == cps) return true;
-    if (bs[0][0] == cps && bs[0][1] == cps && bs[0][2] == cps) return true;
-    if (bs[1][0] == cps && bs[1][1] == cps && bs[1][2] == cps) return true;
-    if (bs[2][0] == cps && bs[2][1] == cps && bs[2][2] == cps) return true;
-  };
-
-  const makeMove = (a, b) => {
-    if (board[a][b] == 'X' || board[a][b] == 'O') {
-      console.log('Not empty');
-      players.switchPlayer();
-    } else {
-      board[a][b] = players.getActivePlayer().playerSymbol;
-      gameBoard.printBoard();
-    };
-
-    if (checkResult(board, players.getActivePlayer())) console.log(`${players.getActivePlayer().name} won! Reload for new game.`);
-    else {
-      players.switchPlayer();
-      console.log(`${players.getActivePlayer().name}'s turn`);
-    };
-  };
-  return { makeMove };
-};
-
-const game = gameFlow(); // game in console
-
