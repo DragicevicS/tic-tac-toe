c61d1c0cd4a2d774538ead31931f938f69eb59d3
DragicevicS <steffan95@live.com>
2023-04-18 21:25:08 +0200

    Make a custom alert box for declaring winners with an option to play another round or to return to main menu

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 const settings = (() => { // starting screen settings for adding player names and choosing symbols
 const form = document.querySelector('form');
+const playerTurn = document.querySelector('.player-turn');
+const boardContainer = document.querySelector('.board-container');
 function switchDisplay(e) {
-  const playerTurn = document.querySelector('.player-turn');
-  const boardContainer = document.querySelector('.board-container');
   e.preventDefault();
   form.style.display = 'none'
   boardContainer.style.display = 'grid';
@@ -44,7 +44,8 @@ const game = () => {
   const playerTurn = document.querySelector('.player-turn');
   const boardContainer = document.querySelector('.board-container');
 
-  let activePlayer = player[0]; 
+  let activePlayer = player[0];
+  if (activePlayer.playerSymbol == 'O') activePlayer = player[1]; // statement for 'X' being the first one to make a move
   const switchPlayer = () => activePlayer = activePlayer === player[0] ? player[1] : player[0];
   const getActivePlayer = () => activePlayer;
   playerTurn.textContent = `${getActivePlayer().name}'s turn - ${getActivePlayer().playerSymbol}`;
@@ -54,26 +55,81 @@ const game = () => {
     cellButton.setAttribute('id', i);
     boardContainer.appendChild(cellButton);
   };
-  
-  function checkResult() { // function with game logic and checking if there is a winner or a draw
+
+  const alertDiv = document.querySelector('.alert');
+  function checkResult() { // function with game logic and checking if there is a winner or a draw, and also declaring result
+    const alertP = document.querySelector('.alert-para');
     let bs = boardContainer.children;
     let cps = getActivePlayer().playerSymbol;
-    function alertWinner() {
+    function alertWinner() { // custom alert box for declaring result
       switchPlayer();
+      boardContainer.removeEventListener('click', clickHandlerBoard);
+      boardContainer.classList.add('disabled', 'alert-opacity-low');
       setTimeout(() => {
-        alert(`${getActivePlayer().name} won!`);
-        location.reload();
-      }, 50);
+      playerTurn.textContent = '';
+      alertDiv.style.display = 'flex';
+      alertDiv.classList.add('alert-opacity-high');
+      alertP.textContent = `${getActivePlayer().name} won!`;
+      }, 300);
     };
-    if (bs[0].textContent == cps && bs[4].textContent == cps && bs[8].textContent == cps) return alertWinner();
-    if (bs[2].textContent == cps && bs[4].textContent == cps && bs[6].textContent == cps) return alertWinner();
-    if (bs[0].textContent == cps && bs[1].textContent == cps && bs[2].textContent == cps) return alertWinner();
-    if (bs[3].textContent == cps && bs[4].textContent == cps && bs[5].textContent == cps) return alertWinner();
-    if (bs[6].textContent == cps && bs[7].textContent == cps && bs[8].textContent == cps) return alertWinner();
-    if (bs[0].textContent == cps && bs[3].textContent == cps && bs[6].textContent == cps) return alertWinner();
-    if (bs[1].textContent == cps && bs[4].textContent == cps && bs[7].textContent == cps) return alertWinner();
-    if (bs[2].textContent == cps && bs[5].textContent == cps && bs[8].textContent == cps) return alertWinner();
-    else if (bs[0].textContent != '' && bs[1].textContent != '' && bs[2].textContent != '' && bs[3].textContent != '' && bs[4].textContent != '' && bs[5].textContent != '' && bs[6].textContent != '' && bs[7].textContent != '' && bs[8].textContent != '') return setTimeout(() => {alert(`It' a draw!`); location.reload();}, 50);
+    if (bs[0].textContent == cps && bs[4].textContent == cps && bs[8].textContent == cps) {
+      bs[0].style.backgroundColor = '#26734d';
+      bs[4].style.backgroundColor = '#26734d';
+      bs[8].style.backgroundColor = '#26734d';
+      return alertWinner();
+    };
+    if (bs[2].textContent == cps && bs[4].textContent == cps && bs[6].textContent == cps) {
+      bs[2].style.backgroundColor = '#26734d';
+      bs[4].style.backgroundColor = '#26734d';
+      bs[6].style.backgroundColor = '#26734d';
+      return alertWinner();
+    };
+    if (bs[0].textContent == cps && bs[1].textContent == cps && bs[2].textContent == cps) {
+      bs[0].style.backgroundColor = '#26734d';
+      bs[1].style.backgroundColor = '#26734d';
+      bs[2].style.backgroundColor = '#26734d';
+      return alertWinner();
+    };
+    if (bs[3].textContent == cps && bs[4].textContent == cps && bs[5].textContent == cps) {
+      bs[3].style.backgroundColor = '#26734d';
+      bs[4].style.backgroundColor = '#26734d';
+      bs[5].style.backgroundColor = '#26734d';
+      return alertWinner();
+    };
+    if (bs[6].textContent == cps && bs[7].textContent == cps && bs[8].textContent == cps) {
+      bs[6].style.backgroundColor = '#26734d';
+      bs[7].style.backgroundColor = '#26734d';
+      bs[8].style.backgroundColor = '#26734d';
+      return alertWinner();
+    };
+    if (bs[0].textContent == cps && bs[3].textContent == cps && bs[6].textContent == cps) {
+      bs[0].style.backgroundColor = '#26734d';
+      bs[3].style.backgroundColor = '#26734d';
+      bs[6].style.backgroundColor = '#26734d';
+      return alertWinner();
+    };
+    if (bs[1].textContent == cps && bs[4].textContent == cps && bs[7].textContent == cps) {
+      bs[1].style.backgroundColor = '#26734d';
+      bs[4].style.backgroundColor = '#26734d';
+      bs[7].style.backgroundColor = '#26734d';
+      return alertWinner();
+    };
+    if (bs[2].textContent == cps && bs[5].textContent == cps && bs[8].textContent == cps) {
+      bs[2].style.backgroundColor = '#26734d';
+      bs[5].style.backgroundColor = '#26734d';
+      bs[8].style.backgroundColor = '#26734d';
+      return alertWinner();
+    }
+    else if (bs[0].textContent != '' && bs[1].textContent != '' && bs[2].textContent != '' && bs[3].textContent != '' && bs[4].textContent != '' && bs[5].textContent != '' && bs[6].textContent != '' && bs[7].textContent != '' && bs[8].textContent != '') {
+      boardContainer.removeEventListener('click', clickHandlerBoard);
+      boardContainer.classList.add('disabled', 'alert-opacity-low');
+      setTimeout(() => {
+      playerTurn.textContent = '';
+      alertDiv.style.display = 'flex';
+      alertDiv.classList.add('alert-opacity-high');
+      alertP.textContent = `It's a draw!`;
+      }, 300);
+    };
   };
   const makeMove = (a) => { // core function for every move, with game logic
     if (boardContainer.children[a].textContent == 'X' || boardContainer.children[a].textContent == 'O') { // checks if the cell is empty
@@ -93,4 +149,13 @@ const game = () => {
     makeMove(selectedCell);
   };
   boardContainer.addEventListener('click', clickHandlerBoard);
+
+  const playAgain = document.querySelector('.play-again');
+  function playAnotherRound() { // playing another round with the same players
+  alertDiv.style.display = 'none';
+  boardContainer.textContent = '';
+  boardContainer.classList.remove('disabled', 'alert-opacity-low');
+  game();
+  };
+  playAgain.addEventListener('click', playAnotherRound);
 };
